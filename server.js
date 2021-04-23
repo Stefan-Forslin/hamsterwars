11333329a3af377e17b3ee085e0c6fd9bb9aab65
Stefan Forslin <stefan.forslin@iths.se>
2021-04-23 23:09:40 +0200

    la till filer, modifierade filer

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const path = require('path')
 const PORT = 1342
+const hamsters = require('./routes/hamsters.js')
 const staticFolder = path.join(__dirname, 'public')
 app.use( express.json())
 app.use( cors())
@@ -14,6 +15,7 @@ app.use((req, res, next) =>{
 app.get('/', (req, res) => {
 	res.send('Welcome to hamsterwars')
 })
+app.use('/hamsters', hamsters)
 app.listen(PORT, () => {
 	console.log('Server listening on port ' + PORT);
 })
