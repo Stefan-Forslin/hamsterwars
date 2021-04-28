cbdfd942f195deb0359584bebb77d76f9415b089
Stefan Forslin <stefan.forslin@iths.se>
2021-04-28 17:11:14 +0200

    made changes

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -23,33 +23,87 @@ router.get('/:id', async (req, res) =>{
 	const data = docRef.data()
 	res.send(data)
 })
-router.post('/', async (req, res) =>{
-	const object = req.body
-	if( object || !id){
-	   res.sendStatus(400)
-	   return
-   }
-	const docRef = await db.collection('Hamsters').add(object)
-	res.send(docRef.id)
-})
-router.put('/:id', async (req, res) =>{
-	const object = req.body
-	const id = req.params.id
-	if( object || !id ){
-	   res.sendStatus(400)
-	   return
-   }
-   const docRef = db.collection('Hamsters').doc(id)
-   await docRef.set(object, {merge:true})
-   res.sendStatus(200)
-})
-function isHamstersObject(maybeObject) {
-	if (!maybeObject )
-	return false
-	else if (!maybeObject.name || !maybeObject.imgName || !maybeObject.defeats || !object.loves || !maybeObject.games || !maybeObject.age || !maybeObject.wins || !maybeObject.favFood)
-	return false
-	return true
-}
+router.post('/', async (req, res) => {
+	const object = req.body;
+	if(!objectRef(object) || Object.keys(object).length === 0) {
+		res.sendStatus(400);
+		return;
+	}
+	let docRef;
+	try {
+		docRef = await db.collection('Hamsters').add(object);
+		const newObj = { id: docRef.id };
+		res.send(newObj);
+	}
+	catch(error) {
+		res.status(500).send(error.message);
+	}
+});
+function objectRef(testObject) {
+	if(
+		testObject && ['name', 'age', 'favFood', 'loves', 'imgName', 'wins', 'defeats', 'games'].every(t => testObject.hasOwnProperty(t)) ) {
+		if (testObject.age < 0 || !Number.isInteger(testObject.age)) return false;
+		if (!Number.isInteger(testObject.wins)) return false;
+		if (!Number.isInteger(testObject.defeats)) return false;
+		if (!Number.isInteger(testObject.games)) return false;
+		return true;
+	}
+	return false;
+};
+router.put('/:id', async (req, res) => {
+	const id = req.params.id;
+	const object = req.body;
+	if(!object || !id) {
+		res.sendStatus(400);
+		return;
+	}
+	const docRef = db.collection('Hamsters').doc(id);
+	let hamsterRef;
+	try {
+		hamsterRef = await docRef.get();
+	}
+	catch(error) {
+		res.status(500).send(error.message);
+		return;
+	}
+	if(!hamsterRef.exists) {
+		res.status(404).send("Hamster does not exits.");
+		return;
+	}
+	try {
+		await docRef.set(object, { merge: true });
+		if(Object.keys(object).length === 0) {
+			res.sendStatus(400);
+			return;
+		}
+		res.sendStatus(200);
+	}
+	catch(error) {
+		res.status(500).send(error.message);
+	}
+});
+router.get('/random', async (req, res) => {
+	let randomRef;
+	try {
+		randomRef = await db.collection('Hamsters').get();
+	}
+	catch(error) {
+		res.status(500).send(error.message);
+	}
+	if (randomRef.empty) {
+		res.send([
+		]);
+		return;
+	}
+	let randomHamster;
+	randomRef.forEach(doc => {
+		const data = doc.data();
+		data.id = doc.id;
+		items.push(data);
+		randomHamster = items[Math.floor(Math.random() * items.length)];
+	});
+	res.send(randomHamster);
+});
 router.delete('/:id', async (req, res) =>{
 	const id = req.params.id
 	if(!id){
@@ -60,3 +114,10 @@ router.delete('/:id', async (req, res) =>{
 	res.sendStatus(200)
 })
 module.exports = router
+/*function isHamstersObject(maybeObject) {
+	if (!maybeObject )
+	return false
+	else if (!maybeObject.name || !maybeObject.imgName || !maybeObject.defeats || !object.loves || !maybeObject.games || !maybeObject.age || !maybeObject.wins || !maybeObject.favFood)
+	return false
+	return true
+}*/
